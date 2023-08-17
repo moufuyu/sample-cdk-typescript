00eeb07504ef4814329200776e7b1ad6443a0e2b
moufuyu <62968285+moufuyu@users.noreply.github.com>
2023-08-18 00:57:57 +0900

    first commit

--- lib/sample_cdk-stack.ts
+++ lib/sample_cdk-stack.ts
@@ -1,16 +1,19 @@
-import * as cdk from 'aws-cdk-lib';
+import { Duration, Stack, StackProps} from 'aws-cdk-lib';
+import {Function, Runtime, AssetCode} from "aws-cdk-lib/aws-lambda";
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class SampleCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class SampleCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SampleCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Function(this, "HelloWorld",{
+      functionName: 'HelloWorld',
+      handler: "handler.handler",
+      runtime: Runtime.NODEJS_14_X,
+      code: new AssetCode('./src'), // lambda code path
+      memorySize: 512,
+      timeout: Duration.seconds(10),
+    })
   }
 }
